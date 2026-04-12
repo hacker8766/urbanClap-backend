71995840f14c59590b786edadae560197a5e2735
hacker8766 <153456694+hacker8766@users.noreply.github.com>
2026-04-12 17:11:54 +0530

    Update cors origin

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend.vercel.app" // 👉 replace with your real frontend URL
+      
+      "https://urbanclap-frontend.vercel.app/" // 👉 replace with your real frontend URL
     ],
     credentials: true,
   })
